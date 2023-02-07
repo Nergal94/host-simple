c1a2d623f3ec23c69bd3a1ab82193a79b22c546d
kostm <anduser@k-merkulov.local>
2023-02-07 12:51:58 +0200

    added emits and props

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,29 @@
       <Button />
     </Section>
     <RemoteSection>
-      <RemoteButton />
+      <RemoteButton msg="test props" @increase="getCounter" />
     </RemoteSection>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import {Vue, Component } from "vue-property-decorator";
 import Section from './components/Section.vue'
 import RButton from 'remote-simple/remote-simple-button';
 import RSection from 'remote-simple/remote-simple-section'
-export default {
-  name: 'App',
+import Button from './components/Button.vue'
+
+@Component({
   components: {
     Section,
-    Button: () => import('./components/Button.vue'),
+    Button,
     RemoteButton: RButton,
     RemoteSection: RSection
+  }
+})
+export default class App extends Vue {
+  public getCounter(count: number) {
+    console.log(count);
   }
 }
 </script>
